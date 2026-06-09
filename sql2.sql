a6f4484aaeee6cf0d43ff4badd48aed89ddce18a
dipanshu2707 <singhdp2707@gmail.com>
2026-06-09 20:34:06 +0530

    add sql2,sql3 and sql4

---
+++ sql2.sql
@@ -0,0 +1,27 @@
+create database college;
+use college;
+
+create table children(
+  rollno int primary key,
+  name varchar(50)
+  );
+  INSERT INTO children
+  (rollno, name)
+  values
+  (102,"ram"),
+  (103,"karan"),
+  (104,"arjun" );
+  
+  
+  
+  select * from children;
+  
+  
+  INSERt INTO CHILDREN 
+  (rollno, name)
+  values
+  (105,"shyan");  
+  
+ 
+ 
+ 
